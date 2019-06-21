7eae96b08a99d78fffd8e1fabee9b26f6f878c35
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-06-21 07:50:57 +0200

    v.1.2.1 - Fixed bug with Sticky Hand.

--- src/main/java/energizedSpire/patches/StickyHandCardRewardScreenCardSelectPatch.java
+++ src/main/java/energizedSpire/patches/StickyHandCardRewardScreenCardSelectPatch.java
@@ -1,5 +1,6 @@
 package energizedSpire.patches;
 
+import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -17,13 +18,18 @@ public class StickyHandCardRewardScreenCardSelectPatch {
     @SpireInsertPatch(locator = Locator.class, localvars = "hoveredCard")
     public static void Insert(CardRewardScreen cardRewardScreen, AbstractCard hoveredCard) {
         if (AbstractDungeon.player.hasRelic(StickyHand.ID)) {
-            for (AbstractCard card : cardRewardScreen.rewardGroup) {
-                if (hoveredCard != null) {
-                    card.drawScale = 1.0f;
-                    card.targetDrawScale = 1.0f;
-                } else {
-                    card.drawScale = MathHelper.cardScaleLerpSnap(card.drawScale, card.targetDrawScale);
-                    card.targetDrawScale = 0.75f;
+            boolean codex = (boolean) ReflectionHacks.getPrivate(cardRewardScreen, CardRewardScreen.class, "codex");
+            boolean discovery = (boolean) ReflectionHacks.getPrivate(cardRewardScreen, CardRewardScreen.class, "discovery");
+            boolean draft = (boolean) ReflectionHacks.getPrivate(cardRewardScreen, CardRewardScreen.class, "draft");
+            if (!codex && !discovery && !draft) {
+                for (AbstractCard card : cardRewardScreen.rewardGroup) {
+                    if (hoveredCard != null) {
+                        card.drawScale = 1.0f;
+                        card.targetDrawScale = 1.0f;
+                    } else {
+                        card.drawScale = MathHelper.cardScaleLerpSnap(card.drawScale, card.targetDrawScale);
+                        card.targetDrawScale = 0.75f;
+                    }
                 }
             }
         }
