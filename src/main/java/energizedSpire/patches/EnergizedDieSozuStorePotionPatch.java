fc351f468cb17ee4da4567628f145267a7e0c7dc
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2019-07-01 23:22:07 +0200

    v.1.2.2 - Updated to Slay the Spire v.1.1.

--- src/main/java/energizedSpire/patches/EnergizedDieSozuStorePotionPatch.java
+++ src/main/java/energizedSpire/patches/EnergizedDieSozuStorePotionPatch.java
@@ -1,22 +1,18 @@
 package energizedSpire.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
-import com.megacrit.cardcrawl.actions.common.ObtainPotionAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.shop.StorePotion;
 import energizedSpire.enums.EnergizedDieEffect;
 import energizedSpire.relics.EnergizedDie;
 import javassist.CtBehavior;
 
-import java.lang.reflect.Method;
-
-@SpirePatch(clz = StorePotion.class, method = "update")
+@SpirePatch(clz = StorePotion.class, method = "purchasePotion")
 public class EnergizedDieSozuStorePotionPatch {
 
     @SpireInsertPatch(locator = Locator.class)
-    public static SpireReturn Insert(StorePotion storePotion, float rugY) {
+    public static SpireReturn Insert(StorePotion storePotion) {
         EnergizedDie energizedDieRelic = (EnergizedDie) AbstractDungeon.player.getRelic(EnergizedDie.ID);
         if (energizedDieRelic != null && energizedDieRelic.isCurrentEffectEqualTo(EnergizedDieEffect.SOZU)) {
             energizedDieRelic.flash();
