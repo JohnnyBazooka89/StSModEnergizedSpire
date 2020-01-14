223231535f0bdbaab084a542178c69fa01f95457
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2020-01-14 23:38:11 +0100

    v.1.5.0 - Fixed Reception Problems.

--- src/main/java/energizedSpire/cards/ReceptionProblems.java
+++ src/main/java/energizedSpire/cards/ReceptionProblems.java
@@ -2,7 +2,6 @@ package energizedSpire.cards;
 
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -18,7 +17,7 @@ public class ReceptionProblems extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 0;
+    private static final int COST = -2;
     private static final int FOCUS_TO_LOSE_AND_REGAIN = 2;
 
     public ReceptionProblems() {
@@ -26,7 +25,6 @@ public class ReceptionProblems extends CustomCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ChangeStanceAction("Depression"));
     }
 
     @Override
